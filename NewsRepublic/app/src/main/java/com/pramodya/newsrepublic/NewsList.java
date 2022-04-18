c364287f1717d19077ecf95ad0a0031a7b1669a5
JasmineK <kahlonjasmine3@gmail.com>
2022-04-18 11:55:20 -0600

    Add List View

--- NewsRepublic/app/src/main/java/com/pramodya/newsrepublic/NewsList.java
+++ NewsRepublic/app/src/main/java/com/pramodya/newsrepublic/NewsList.java
@@ -1,18 +1,14 @@
 package com.pramodya.newsrepublic;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.database.sqlite.SQLiteStatement;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -20,14 +16,17 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
-
 public class NewsList extends AppCompatActivity {
-
-
+    ListView listView;
+    static ArrayList<String> newsTitles =new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_list);
-
+        listView = findViewById(R.id.newsList);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, newsTitles);
+        listView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
     }
+
 }
