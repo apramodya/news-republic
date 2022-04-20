312b931de33a7df210c5a3ceab53d91dbd023238
Pramodya <pramodyaabeysinghe@gmail.com>
2022-04-19 21:18:39 -0600

    change categories

--- NewsRepublic/app/src/main/java/com/pramodya/newsrepublic/MainActivity.java
+++ NewsRepublic/app/src/main/java/com/pramodya/newsrepublic/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +16,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onCategorySelect(View view) {
+        Button button = (Button) view;
         Intent intent = new Intent(this, NewsList.class);
+        intent.putExtra("title", button.getText().toString());
         startActivity(intent);
     }
 
